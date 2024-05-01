ff00528e5a5523d5ebcd4307cfefd1d24df1923d
Karina <129091450+karina725@users.noreply.github.com>
2024-05-01 13:14:24 -0600

    Update auth.dart

--- basededatos/lib/utils/auth.dart
+++ basededatos/lib/utils/auth.dart
@@ -1,5 +1,5 @@
 
-import 'package:firebase_auth/firebase_auth.dart'; // Asegúrate de importar el archivo correcto para la página del dashboard
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
